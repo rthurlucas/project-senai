2e9beaf47f639fff6f043be6e94dd8c3b0070ea0
rthurlucas <arthurlucasx696@gmail.com>
2026-04-22 15:43:54 -0300

    refactor: criando e adicionando metodo de cadastro

--- project-senai/src/main/java/br/com/cantina/senai/service/UsuarioService.java
+++ project-senai/src/main/java/br/com/cantina/senai/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package br.com.cantina.senai.service;
 
+import br.com.cantina.senai.model.Usuario;
 import br.com.cantina.senai.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UsuarioService {
@@ -12,5 +16,10 @@ public class UsuarioService {
     @Autowired
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
+    }
+
+    @Transactional
+    public void cadastrarUsuario(@RequestBody @Valid Usuario dados){
+
     }
 }
