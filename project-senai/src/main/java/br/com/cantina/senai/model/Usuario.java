cdadcab054d359209c2c45fbbfbabd2c7149bcae
Be1506 <brenooaugusto21@gmail.com>
2026-04-22 14:51:01 -0300

    conexão feita e a tabela de usuario também!

--- project-senai/src/main/java/br/com/cantina/senai/model/Usuario.java
+++ project-senai/src/main/java/br/com/cantina/senai/model/Usuario.java
@@ -1,4 +1,15 @@
 package br.com.cantina.senai.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+
 }
