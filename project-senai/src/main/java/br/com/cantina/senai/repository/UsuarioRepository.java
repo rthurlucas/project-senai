07dc968c4f7e76653b167ce1f8e8c0f00eb3876e
rthurlucas <arthurlucasx696@gmail.com>
2026-04-22 16:50:48 -0300

    feat: adicionando o List findAllById

--- project-senai/src/main/java/br/com/cantina/senai/repository/UsuarioRepository.java
+++ project-senai/src/main/java/br/com/cantina/senai/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package br.com.cantina.senai.repository;
 import br.com.cantina.senai.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+    List<Usuario> findAllById(Long id);
 }
