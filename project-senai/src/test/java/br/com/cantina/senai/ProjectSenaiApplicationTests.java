134dacb030a2908074c5cb4c333206fe3b036095
rthurlucas <arthurlucasx696@gmail.com>
2026-04-22 13:28:21 -0300

    Initial commit

---
+++ project-senai/src/test/java/br/com/cantina/senai/ProjectSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cantina.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
