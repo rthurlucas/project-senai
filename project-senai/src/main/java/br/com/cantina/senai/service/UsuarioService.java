831c101560053decf82a1acbe47f3d941f2f0401
rthurlucas <arthurlucasx696@gmail.com>
2026-04-22 16:50:11 -0300

    feat:adicionando o metodo excluir

--- project-senai/src/main/java/br/com/cantina/senai/service/UsuarioService.java
+++ project-senai/src/main/java/br/com/cantina/senai/service/UsuarioService.java
@@ -1,12 +1,17 @@
 package br.com.cantina.senai.service;
 
+import br.com.cantina.senai.model.DTOCadastroUsuario;
 import br.com.cantina.senai.model.Usuario;
 import br.com.cantina.senai.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -19,7 +24,22 @@ public class UsuarioService {
     }
 
     @Transactional
-    public void cadastrarUsuario(@RequestBody @Valid Usuario dados){
+    public Usuario cadastrarUsuario(@RequestBody @Valid Usuario dados){
+        Usuario usuario = new Usuario(dados);
+        usuarioRepository.save(usuario);
+        return usuarioRepository.save(dados);
+    }
 
+    @Transactional
+    public List<Usuario> listarUsuarios(@PathVariable Long id){
+        Usuario usuario = usuarioRepository.getReferenceById(id);
+        return usuarioRepository.findAllById(id);
+    }
+
+    @Transactional
+    public void excluirUsuarios(Long id){
+        Usuario usuario = new Usuario();
+        usuario.excluir();
+        return usuarioRepository.delete();
     }
 }
