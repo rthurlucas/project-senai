f7beec02351dd4fb24ecd42d75ff3fe0351b2be2
rthurlucas <arthurlucasx696@gmail.com>
2026-04-23 14:23:45 -0300

    feat: criação dos metodos, listarPedido, buscarPedido, excluirPedido

--- project-senai/src/main/java/br/com/cantina/senai/service/PedidoService.java
+++ project-senai/src/main/java/br/com/cantina/senai/service/PedidoService.java
@@ -2,12 +2,16 @@ package br.com.cantina.senai.service;
 
 import br.com.cantina.senai.model.estoque.Estoque;
 import br.com.cantina.senai.model.pedido.Pedido;
+import br.com.cantina.senai.model.pedido.PedidoNotFound;
 import br.com.cantina.senai.model.pedido.PedidoRepository;
+import br.com.cantina.senai.model.usuario.Usuario;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class PedidoService {
@@ -19,8 +23,26 @@ public class PedidoService {
     }
 
     @Transactional
-    public void criarPedido(@RequestBody @Valid Pedido dados){
+    public void criarPedido(Pedido dados){
         Pedido pedido = new Pedido(dados);
         pedidoRepository.save(pedido);
+    }
+
+    @Transactional
+    public List<Pedido> listarPedido(Long idPedido){
+        return pedidoRepository.findAll();
+    }
+
+    @Transactional
+    public Pedido buscarPedido(Long idPedido){
+        return pedidoRepository.getReferenceById(idPedido);
+    }
+
+    @Transactional
+    public void excluirPedido(Long idPedido){
+        if (!pedidoRepository.existsById(idPedido)){
+            throw new PedidoNotFound("Esse Id nao existe: " + idPedido);
+        }
+        pedidoRepository.deleteById(idPedido);
     }
 }
