112596cd6a918d5cdca369d989e2fe5c9b4e3a73
rthurlucas <arthurlucasx696@gmail.com>
2026-04-23 14:24:54 -0300

    refactor: pacotes concertados

--- project-senai/src/main/java/br/com/cantina/senai/model/pedido/PedidoRepository.java
+++ project-senai/src/main/java/br/com/cantina/senai/model/pedido/PedidoRepository.java
@@ -2,5 +2,8 @@ package br.com.cantina.senai.model.pedido;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findPedidoByIdPedido(Long idPedido);
 }
