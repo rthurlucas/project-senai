7741079efce6ee225f467d1931cac6f05af8b974
rthurlucas <arthulucasx696@gmail.com>
2026-04-22 21:49:01 -0300

    feat: Adicionando @JoinColumn para Produto

--- project-senai/src/main/java/br/com/cantina/senai/model/estoque/Estoque.java
+++ project-senai/src/main/java/br/com/cantina/senai/model/estoque/Estoque.java
@@ -1,5 +1,6 @@
-package br.com.cantina.senai.model;
+package br.com.cantina.senai.model.estoque;
 
+import br.com.cantina.senai.model.produto.Produto;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,12 +12,17 @@ import lombok.*;
 @Getter
 @Setter
 public class Estoque {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idEstoque;
 
-    private Long idProduto;
+    @JoinColumn(name = "id_produto")
+    @ManyToOne
+    private Produto idProduto;
 
     private Integer quantidade;
+
+    public Estoque(Estoque estoque){
+
+    }
 }
