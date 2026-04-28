986950270d581f66c427e92474e4e7729376c781
Be1506 <brenooaugusto21@gmail.com>
2026-04-28 16:01:34 -0300

    feat: Conexão com o banco feita por meio da classe DbConfig.

---
+++ project-senai/src/main/java/br/com/cantina/senai/config/DbConfig.java
@@ -0,0 +1,23 @@
+package br.com.cantina.senai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/");
+        ds.setUsername("root");
+        ds.setPassword("senai@126");
+
+        return ds;
+    }
+
+}
