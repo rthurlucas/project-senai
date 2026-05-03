891feb305a6914cb48eef0ea1243ae41734227df
rthurlucas <arthulucasx696@gmail.com>
2026-05-02 23:40:19 -0300

    refactor: Adicionando @Column no quantidade e LocalDateTime

--- project-senai/src/main/java/br/com/cantina/senai/model/pedido/Pedido.java
+++ project-senai/src/main/java/br/com/cantina/senai/model/pedido/Pedido.java
@@ -1,61 +1,39 @@
 package br.com.cantina.senai.model.pedido;
 
+import br.com.cantina.senai.dto.DTOCadastroPedido;
 import br.com.cantina.senai.model.produto.Produto;
 import br.com.cantina.senai.model.usuario.Usuario;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pedido")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPedido;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_produto")
     private Produto produto;
+    @Column(name = "quantidade")
+    private Integer quantidade;
+    @Column(name = "data_pedido")
+    private LocalDateTime dataPedido;
 
-    public Pedido(Pedido pedido){
-
-    }
-
-    public void atualizarInformacoesPedido(Pedido pedido){
-
-    }
-
-    public Pedido(Long idPedido, Usuario usuario, Produto produto) {
-        this.idPedido = idPedido;
-        this.usuario = usuario;
-        this.produto = produto;
-    }
-
-    public Pedido() {
-    }
-
-    public Long getIdPedido() {
-        return idPedido;
-    }
-
-    public void setIdPedido(Long idPedido) {
-        this.idPedido = idPedido;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
+    public Pedido(DTOCadastroPedido dados) {
+        this.quantidade = quantidade;
+        this.dataPedido = dataPedido;
     }
 }
