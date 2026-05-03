3d2881c4f172ded8a933d502df151b05030b3986
rthurlucas <arthulucasx696@gmail.com>
2026-05-02 23:30:38 -0300

    feat: corrigindo buscarProdutoPorId, adicionando o buscarProdutoNome, atualizarNomeProduto

--- project-senai/src/main/java/br/com/cantina/senai/service/ProdutoService.java
+++ project-senai/src/main/java/br/com/cantina/senai/service/ProdutoService.java
@@ -1,7 +1,10 @@
 package br.com.cantina.senai.service;
 
-
+import br.com.cantina.senai.dto.DTOAtualizarProduto;
+import br.com.cantina.senai.dto.DTOCadastroProduto;
+import br.com.cantina.senai.dto.DTODetalhamentoProduto;
 import br.com.cantina.senai.model.produto.Produto;
+import br.com.cantina.senai.model.produto.ProdutoNameException;
 import br.com.cantina.senai.model.produto.ProdutoNotFoundException;
 import br.com.cantina.senai.model.produto.ProdutoRepository;
 import jakarta.transaction.Transactional;
@@ -21,20 +24,35 @@ public class ProdutoService {
     }
 
     @Transactional
-    public Produto criarDescricaoPedido(Produto dados){
-        Produto produtoCriar = new Produto(dados);
-        produtoRepository.save(produtoCriar);
-        return produtoCriar;
+    public DTODetalhamentoProduto criarDescricaoProduto(DTOCadastroProduto dados){
+        Produto produto = new Produto(dados);
+        produtoRepository.save(produto);
+        return new DTODetalhamentoProduto(produto);
     }
 
-    @Transactional
     public List<Produto> listarProduto(Long id){
         return produtoRepository.findAll();
     }
 
-    @Transactional
-    public Produto buscarProdutoPorId(Long id){
-        return produtoRepository.findById(id).orElseThrow(() -> new ProdutoNotFoundException("Produto nao encontrado para buscar ID: " + id));
+    public DTODetalhamentoProduto buscarProdutoPorId(Long idProduto){
+        Produto produto = produtoRepository.findById(idProduto)
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto nao encontrado para buscar ID: " + idProduto));
+        return new DTODetalhamentoProduto(produto);
+    }
+
+    public DTODetalhamentoProduto buscarProdutoPorNome(String nomeProduto){
+        Produto produto = produtoRepository.findByNomeProduto(nomeProduto).orElseThrow(() -> new ProdutoNameException("Produto nao encontrado pelo nome: " + nomeProduto));
+        return new DTODetalhamentoProduto(produto);
+    }
+
+    public DTODetalhamentoProduto atualizarNomeProduto(DTOAtualizarProduto dados, Long idProduto){
+        Produto produto = produtoRepository.findById(idProduto)
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto nao encontrado pelo ID: " + idProduto));
+        if (dados.descricaoProduto() != null){
+            produto.setDescricaoProduto(dados.descricaoProduto());
+        }
+       produto.setNomeProduto(dados.nomeProduto());
+        return new  DTODetalhamentoProduto(produto);
     }
 
     @Transactional
