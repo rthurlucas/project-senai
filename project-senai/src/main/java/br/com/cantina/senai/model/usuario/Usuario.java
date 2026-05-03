d0cec39a63431cad92abb0e1d2a39f023d0c524c
rthurlucas <arthulucasx696@gmail.com>
2026-05-02 23:25:00 -0300

    fix: implementando novamente o @Lombok, coloando @Column para o Hibernate não dar conflito nas tabelas.

--- project-senai/src/main/java/br/com/cantina/senai/model/usuario/Usuario.java
+++ project-senai/src/main/java/br/com/cantina/senai/model/usuario/Usuario.java
@@ -1,70 +1,40 @@
 package br.com.cantina.senai.model.usuario;
 
 
+import br.com.cantina.senai.dto.DTOCadastroUsuario;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
     private Long id;
+    @Column(name = "nome")
     private String nome;
+    @Column(name = "cpf")
     private String cpf;
+    @Column(name = "telefone")
     private String telefone;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo")
-    public TipoUsuario tipoUsuario;
+    private TipoUsuario tipoUsuario;
 
-    public Usuario(Usuario dados) {
-    }
+  public Usuario(DTOCadastroUsuario usuario){
+
+  }
 
     public void excluir(){
         this.nome = "Unknow";
-    }
-
-    public Usuario(Long id, String nome, String cpf, String telefone) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.telefone = telefone;
-    }
-
-    public Usuario() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
     }
 }
