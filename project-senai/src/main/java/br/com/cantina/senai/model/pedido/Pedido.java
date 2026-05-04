4a371cdfb7d9e6612a03be604be72ae3901b8ba2
rthurlucas <arthulucasx696@gmail.com>
2026-05-04 16:56:00 -0300

    refactor: renomear atributo quantidade para quantidadePedido na classe Pedido

--- project-senai/src/main/java/br/com/cantina/senai/model/pedido/Pedido.java
+++ project-senai/src/main/java/br/com/cantina/senai/model/pedido/Pedido.java
@@ -4,8 +4,6 @@ import br.com.cantina.senai.dto.DTOCadastroPedido;
 import br.com.cantina.senai.model.produto.Produto;
 import br.com.cantina.senai.model.usuario.Usuario;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -17,7 +15,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Pedido {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPedido;
@@ -28,12 +25,12 @@ public class Pedido {
     @JoinColumn(name = "id_produto")
     private Produto produto;
     @Column(name = "quantidade")
-    private Integer quantidade;
+    private Integer quantidadePedido;
     @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
 
     public Pedido(DTOCadastroPedido dados) {
-        this.quantidade = quantidade;
+        this.quantidadePedido = quantidadePedido;
         this.dataPedido = dataPedido;
     }
 }
