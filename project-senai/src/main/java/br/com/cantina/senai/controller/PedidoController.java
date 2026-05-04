c54277208974e1d6f262d7f73a26a5acde5c89fd
rthurlucas <arthulucasx696@gmail.com>
2026-05-04 16:54:57 -0300

    feat: implementar HomeController e integrar frontend com backend para gerenciamento de pedidos

--- project-senai/src/main/java/br/com/cantina/senai/controller/PedidoController.java
+++ project-senai/src/main/java/br/com/cantina/senai/controller/PedidoController.java
@@ -1,4 +1,27 @@
 package br.com.cantina.senai.controller;
 
+import br.com.cantina.senai.dto.DTOCadastroPedido;
+import br.com.cantina.senai.dto.DTODetalhamentoPedido;
+import br.com.cantina.senai.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/pedidos")
 public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    @Autowired
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<DTODetalhamentoPedido> criarPedido(
+            @RequestBody DTOCadastroPedido dados) {
+        DTODetalhamentoPedido pedido = pedidoService.criarPedido(dados, dados.idUsuario());
+        return ResponseEntity.status(201).body(pedido);
+    }
 }
