e7653fc3cf14181f8d10c432f224ac0094c9bdad
rthurlucas <arthulucasx696@gmail.com>
2026-05-05 11:21:09 -0300

    fix: correção de bug na parte do controller e mudança de endpoint

--- project-senai/src/main/java/br/com/cantina/senai/controller/ProdutoController.java
+++ project-senai/src/main/java/br/com/cantina/senai/controller/ProdutoController.java
@@ -1,17 +1,19 @@
 package br.com.cantina.senai.controller;
 
 import br.com.cantina.senai.dto.DTOCadastroProduto;
+import br.com.cantina.senai.dto.DTOAtualizarProduto;
 import br.com.cantina.senai.dto.DTODetalhamentoProduto;
 import br.com.cantina.senai.dto.DTOListagemProduto;
 import br.com.cantina.senai.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/produtos")
+@Controller
+@RequestMapping
 public class ProdutoController {
 
     private final ProdutoService produtoService;
@@ -21,21 +23,43 @@ public class ProdutoController {
         this.produtoService = produtoService;
     }
 
-    @GetMapping
+
+    @GetMapping("/produto/listar")
     public ResponseEntity<List<DTOListagemProduto>> listarProdutos() {
         List<DTOListagemProduto> produtos = produtoService.listarProdutos();
         return ResponseEntity.ok(produtos);
     }
 
-    @GetMapping("/{Id}")
-    public ResponseEntity<DTODetalhamentoProduto> buscarProdutoId(@PathVariable Long idProduto) {
-        DTODetalhamentoProduto produto = produtoService.buscarProdutoPorId(idProduto);
+    @GetMapping("/{id}")
+    public ResponseEntity<DTODetalhamentoProduto> buscarProdutoPorId(@PathVariable Long id) {
+        DTODetalhamentoProduto produto = produtoService.buscarProdutoPorId(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @GetMapping("/produto/{nome}")
+    public ResponseEntity<DTODetalhamentoProduto> buscarProdutoPorNome(@PathVariable String nome) {
+        DTODetalhamentoProduto produto = produtoService.buscarProdutoPorNome(nome);
         return ResponseEntity.ok(produto);
     }
 
     @PostMapping
     public ResponseEntity<DTODetalhamentoProduto> criarProduto(@RequestBody DTOCadastroProduto dados) {
         DTODetalhamentoProduto produto = produtoService.criarDescricaoProduto(dados);
-        return ResponseEntity.ok(produto);
+        return ResponseEntity.status(201).body(produto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DTODetalhamentoProduto> atualizarProduto(
+            @PathVariable Long id,
+            @RequestBody DTOAtualizarProduto dados
+    ) {
+        DTODetalhamentoProduto produtoAtualizado = produtoService.atualizarNomeProduto(dados, id);
+        return ResponseEntity.ok(produtoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        produtoService.excluirProduto(id);
+        return ResponseEntity.noContent().build();
     }
 }
