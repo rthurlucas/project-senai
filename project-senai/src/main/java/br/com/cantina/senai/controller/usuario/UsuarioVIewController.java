9b4d2852b22b57812d043b88fe7989a74d10578f
Arthur <140291017+rthurlucas@users.noreply.github.com>
2026-05-07 12:01:38 -0300

    Update UsuarioVIewController.java

--- project-senai/src/main/java/br/com/cantina/senai/controller/usuario/UsuarioVIewController.java
+++ project-senai/src/main/java/br/com/cantina/senai/controller/usuario/UsuarioVIewController.java
@@ -20,17 +20,13 @@ public class UsuarioVIewController {
 
     @GetMapping("/cadastrar")
     public String exibirCadastro(Model model) {
-        // Isso aqui garante que o formulário não comece quebrado
         model.addAttribute("dto", new DTOCadastroUsuario("", "", "", "", br.com.cantina.senai.model.usuario.TipoUsuario.USUARIO));
         return "cadastro";
     }
 
     @PostMapping("/cadastrar")
     public String processarCadastro(@ModelAttribute("dto") DTOCadastroUsuario dto) {
-        // 1. Salva no banco (Isso resolve o problema do NULL se o DTO estiver com os nomes certos)
         usuarioService.cadastrarUsuario(dto);
-
-        // 2. Redireciona para a Home (Isso resolve o erro 404 de POST /home)
         return "redirect:/home";
     }
 }
